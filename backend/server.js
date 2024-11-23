6de7c56a58c55e03fc360af9701e12a7a5667059
silpa4251 <silpadas4251@gmail.com>
2024-11-23 10:21:36 +0530

    adminroute and corss

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const app = require("./app");
 const connect = require("./config/connection");
 
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 // Database connection
 connect();
+
+// Middleware
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
